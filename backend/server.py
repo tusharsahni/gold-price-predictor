f8baf0642fccc4d887765ebd0b264e3c7d36a429
tusharsahni <tusharsahni1234@gmail.com>
2024-07-26 01:42:25 +0530

    tried button

--- backend/server.py
+++ backend/server.py
@@ -1,9 +1,16 @@
 from flask import Flask, request, jsonify
 import pickle
 import pandas as pd
+from flask_cors import CORS
+import numpy as np
+# import matplotlib.pyplot as plt
+# import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn import metrics
 
 app = Flask(__name__)
-
+CORS(app)
 # Load the model
 model = pickle.load(open('C:/Users/tusha/OneDrive/Desktop/gold price predictor/backend/model.pkl', 'rb'))
 
